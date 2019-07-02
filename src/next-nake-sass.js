8974656d402252401f82cc82621f6e2baa394a9d
afeiship <1290657123@qq.com>
2019-07-02 23:27:47 +0800

    add: basic passed code

--- src/next-nake-sass.js
+++ src/next-nake-sass.js
@@ -2,17 +2,27 @@
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
   var fs = require('fs');
+  var path = require('path');
   var globby = require('globby');
   var sass = require('node-sass');
 
   nx.nakeSass = function(inPatterns, inDest, inOptions) {
-    var files = globby.sync(inPatterns, inOptions);
+    var files = globby.sync(inPatterns);
     files.forEach(function(file) {
-      var content = fs.readFileSync(file).toString();
-      // var dest = path.join(inDest, '/', file);
-      // fs.mkdirSync(path.dirname(dest), { recursive: true });
-      var newContent = sass.renderSync(nx.mix({ data: content }, inOptions));
-      console.log('new Content', newContent.css);
+      var data = fs.readFileSync(file).toString();
+      var dest = path.join(inDest, '/', file).replace('.scss', '.css');
+      var result = sass.renderSync(
+        nx.mix(
+          {
+            data: data,
+            outputStyle: 'compressed'
+          },
+          inOptions
+        )
+      );
+
+      fs.mkdirSync(path.dirname(dest), { recursive: true });
+      fs.writeFileSync(dest, result.css);
     });
   };
 
