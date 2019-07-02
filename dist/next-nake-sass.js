0749ee273ef56bdf1691ad18da481738df7dd7b6
afeiship <1290657123@qq.com>
2019-07-02 23:28:24 +0800

    add: first build

--- dist/next-nake-sass.js
+++ dist/next-nake-sass.js
@@ -1,3 +1,11 @@
+/*!
+ * name: next-nake-sass
+ * url: https://github.com/afeiship/next-nake-sass
+ * version: 1.0.0
+ * date: 2019-07-02T15:28:19.187Z
+ * license: MIT
+ */
+
 (function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
@@ -30,3 +38,5 @@
     module.exports = nx.nakeSass;
   }
 })();
+
+//# sourceMappingURL=next-nake-sass.js.map
